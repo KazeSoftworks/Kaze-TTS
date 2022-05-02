81de04df842128c2ccf516dbc785ca92bd3c7b7e
Spellkaze <spellkaze@live.com>
2022-05-02 17:37:32 -0500

    👽️ Emotesets now require client id, ignoring them for now

--- src/utils/Twitch/chat.js
+++ src/utils/Twitch/chat.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { messagesSlice } from '../Redux/messagesSlice';
 
 export const client = new tmi.Client({
-	options: { debug: true },
+	options: { skipUpdatingEmotesets: true, debug: true },
 	identity: {
 		username: process.env.REACT_APP_CLIENT_USERNAME,
 		password: process.env.REACT_APP_CLIENT_OAUTH,
