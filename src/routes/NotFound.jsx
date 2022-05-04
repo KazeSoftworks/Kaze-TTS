bbc160c8e52c664eedc5f4367eddcff9d259bf02
Spellkaze <spellkaze@live.com>
2022-05-03 23:46:41 -0500

    💄 Not Found Styling

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,10 +1,22 @@
+import HomeIcon from '@mui/icons-material/Home';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../scss/NotFound.scss';
 
 const NotFound = () => {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate('/');
+	};
+
 	return (
-		<div>
-			<h1>404</h1>
-			<h2>Not found</h2>
+		<div className="not-found">
+			<h1 className="not-found__title">404</h1>
+			<h2 className="not-found__subtitle">Not found</h2>
+			<button type="button" className="not-found__button" onClick={handleClick}>
+				<HomeIcon /> <span>Go back to home</span>
+			</button>
 		</div>
 	);
 };
