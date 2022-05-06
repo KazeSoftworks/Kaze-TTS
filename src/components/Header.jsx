ecd50c79598126a5514c26a7ecdb9063a21db6d3
Spellkaze <spellkaze@live.com>
2022-05-05 19:55:47 -0500

    ♻️ Adding new button component to Header and Not Found page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitch } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { AUTH_URI } from '../utils/constants';
 import KazeSymbol from '../assets/KazeSymbol.svg';
 import '../scss/Header.scss';
+import Button from './Button';
 
 const Header = () => {
+	const handleLogin = () => {
+		window.location.href = AUTH_URI;
+	};
 	return (
 		<div className="header">
 			<div className="header__left">
@@ -14,13 +18,13 @@ const Header = () => {
 				<h1>Kaze-TTS</h1>
 			</div>
 			<div className="header__right">
-				<button
-					type="button"
-					className="header__login-button"
-					onClick={() => window.open(AUTH_URI, '_self')}
+				<Button
+					onClick={handleLogin}
+					startIcon={faTwitch}
+					className="button-twitch"
 				>
-					<FontAwesomeIcon icon={faTwitch} /> LOGIN
-				</button>
+					Login
+				</Button>
 				<FontAwesomeIcon className="header__account_logo" icon={faCircleUser} />
 			</div>
 		</div>
--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,6 +1,7 @@
-import HomeIcon from '@mui/icons-material/Home';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
 import '../scss/NotFound.scss';
 
 const NotFound = () => {
@@ -14,9 +15,9 @@ const NotFound = () => {
 		<div className="not-found">
 			<h1 className="not-found__title">404</h1>
 			<h2 className="not-found__subtitle">Not found</h2>
-			<button type="button" className="not-found__button" onClick={handleClick}>
-				<HomeIcon /> <span>Go back to home</span>
-			</button>
+			<Button onClick={handleClick} startIcon={faHome}>
+				Go back to homepage
+			</Button>
 		</div>
 	);
 };
