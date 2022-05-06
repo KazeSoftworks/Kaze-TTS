e6afbee59f96137a174dd597d60ade37357d9c88
Spellkaze <spellkaze@live.com>
2022-05-05 19:54:56 -0500

    ✨ Making a generic Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,46 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { faTwitch } from '@fortawesome/free-brands-svg-icons';
+import '../scss/Button.scss';
 
-const Button = ({ onClick }) => {
+const Button = ({ onClick, className, children, startIcon, endIcon }) => {
 	return (
-		<button type="button" className="header__login-button" onClick={onClick}>
-			<FontAwesomeIcon icon={faTwitch} /> LOGIN
+		<button type="button" className={`button ${className}`} onClick={onClick}>
+			{startIcon && <FontAwesomeIcon icon={startIcon} />}
+			{children}
+			{endIcon && <FontAwesomeIcon icon={endIcon} />}
 		</button>
 	);
 };
 
+const FontAwesomeIconShape = PropTypes.shape({
+	icon: PropTypes.arrayOf(
+		PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.number,
+			PropTypes.arrayOf(
+				PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+			),
+		])
+	),
+	iconName: PropTypes.string,
+	prefix: PropTypes.string,
+});
+
 Button.propTypes = {
 	onClick: PropTypes.func,
+	children: PropTypes.node,
+	className: PropTypes.string,
+	startIcon: FontAwesomeIconShape,
+	endIcon: FontAwesomeIconShape,
 };
 
 Button.defaultProps = {
 	onClick: () => {},
+	children: null,
+	className: '',
+	startIcon: null,
+	endIcon: null,
 };
 
 export default Button;
