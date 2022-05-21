0753bf774aa320075700af5ef75e66a05b04ace5
Spellkaze <spellkaze@live.com>
2022-05-21 17:11:17 -0500

    🚧 Sidemenu adjustments wip

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '@scss/SideMenu.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 const SideMenu = () => {
 	return (
 		<div className="side-menu">
-			{/* <button type="button" className="side-menu__settings-button">
-				Settings
-			</button> */}
+			<button type="button" className="side-menu__settings-button">
+				<FontAwesomeIcon icon={faGear} /> Settings
+			</button>
 		</div>
 	);
 };
