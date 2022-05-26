c333d079a162675ff32ae0f80c977beb5d06cc0e
Spellkaze <spellkaze@live.com>
2022-05-25 19:44:03 -0500

    💥 Move app loading from Home Route to App container. Move tmi client handling to helper function
    Because settings requires all the auth and tmi client load up, all the logic from home needed to be moved to the App layout. In Home it would stop the moment it was changed to settings route

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { Outlet } from 'react-router-dom';
 import '@scss/App.scss';
 import SideMenu from '@components/SideMenu/SideMenu';
+import { validateToken } from '@features/authSlice';
+import {
+	getFollowersInfo,
+	getUserInfo,
+	getGlobalChatBadgesInfo,
+	getChatBadgesInfo,
+} from '@features/twichSlice';
+import setTmiClient from '@utils/chat';
+import {
+	getGlobalBTTVEmotesInfo,
+	getGlobalEmotesInfo,
+	getChannelBttvEmotesInfo,
+} from '@features/messagesSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
+	const dispatch = useDispatch();
+	const { username, token, isAuthenticated } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (token) {
+			dispatch(validateToken());
+		}
+	}, [token, dispatch]);
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			dispatch(getUserInfo())
+				.unwrap()
+				.then(() => {
+					console.log('user info loaded');
+					return dispatch(getFollowersInfo()).unwrap();
+				})
+				.then(() => {
+					console.log('followers info loaded');
+					return dispatch(getGlobalEmotesInfo()).unwrap();
+				})
+				.then(() => {
+					console.log('global emotes info loaded');
+					return dispatch(getGlobalChatBadgesInfo()).unwrap();
+				})
+				.then(() => {
+					console.log('global chat info loaded');
+					return dispatch(getChatBadgesInfo()).unwrap();
+				})
+				.then(() => {
+					console.log('chat badges info loaded');
+					return dispatch(getGlobalBTTVEmotesInfo()).unwrap();
+				})
+				.then(() => {
+					console.log('global BBTV info loaded');
+					return dispatch(getChannelBttvEmotesInfo()).unwrap();
+				})
+				.then(() => {
+					console.log('channel BBTV info loaded');
+					console.log('loading finished');
+					setTmiClient({ username, token, dispatch });
+				});
+		}
+	}, [isAuthenticated, dispatch]);
+
 	return (
 		<>
 			<Header />
--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,6 +1,9 @@
+import { addMessage } from '@features/messagesSlice';
+import { addChatter, removeChatter } from '@features/twichSlice';
 import tmi from 'tmi.js';
+import { parseTwitchMessage } from './messageHandler';
 
-const getClient = (username, token) => {
+const setTmiClient = ({ username, token, dispatch = () => {} }) => {
 	const client = new tmi.Client({
 		options: { skipUpdatingEmotesets: true, debug: true },
 		identity: {
@@ -10,7 +13,32 @@ const getClient = (username, token) => {
 		channels: [username],
 		messagesLogLevel: 'info',
 	});
+	client.connect().catch((err) => {
+		console.error(err);
+	});
+	client.on('message', (__, tags, message, self) => {
+		if (self) {
+			return;
+		}
+		if (tags.username !== username) {
+			dispatch(addChatter(parseTwitchMessage({ tags })));
+		}
+		dispatch(addMessage({ tags, message }));
+	});
+	client.on('join', (__, user, self) => {
+		if (self) {
+			return;
+		}
+		dispatch(addChatter({ username: user, isLurker: true }));
+	});
+	client.on('part', (__, user, self) => {
+		// Ignore echoed messages.
+		if (self) {
+			return;
+		}
+		dispatch(removeChatter(user));
+	});
 	return client;
 };
 
-export default getClient;
+export default setTmiClient;
