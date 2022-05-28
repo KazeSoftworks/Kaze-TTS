409c099e832ab08475be0acf736d5f00cf928723
Spellkaze <spellkaze@live.com>
2022-05-27 21:11:16 -0500

    🚧 Added audio settings route for voice choosing

--- src/routes/VoiceSettings.jsx
+++ src/routes/VoiceSettings.jsx
@@ -1,7 +1,30 @@
+import { setGeneralVoiceIndex } from '@features/settingsSlice';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const VoiceSettings = () => {
-	return <div>VoiceSettings</div>;
+	const voices = useSelector((state) => state.settings.voices);
+	const generalVoiceIndex = useSelector(
+		(state) => state.settings.generalVoiceIndex
+	);
+	const dispatch = useDispatch();
+
+	const handleVoiceChange = (e) => {
+		dispatch(setGeneralVoiceIndex(e.target.value));
+	};
+
+	return (
+		<div>
+			<h1>VoiceSettings</h1>
+			<select value={generalVoiceIndex || ''} onChange={handleVoiceChange}>
+				{voices.map((option, index) => (
+					<option key={`general-voice-${option.name}`} value={index}>
+						{option.name}
+					</option>
+				))}
+			</select>
+		</div>
+	);
 };
 
 export default VoiceSettings;
