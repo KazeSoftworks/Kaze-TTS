b31fe86bd7888ab3ba2d3711aef6740147a669fd
Spellkaze <spellkaze@live.com>
2022-06-03 01:26:31 -0500

    🐛 CSS Minimizer + build:serve missing the -s (static) flag

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,35 @@
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
 	mode: 'production',
 	optimization: {
 		minimize: true,
-		minimizer: [new TerserPlugin({ parallel: true })],
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					parse: {
+						ecma: 8,
+					},
+					compress: {
+						ecma: 5,
+						warnings: false,
+						comparisons: false,
+						inline: 2,
+					},
+					mangle: {
+						safari10: true,
+					},
+					output: {
+						ecma: 5,
+						comments: false,
+						ascii_only: true,
+					},
+				},
+			}),
+			new CssMinimizerPlugin(),
+		],
 	},
 });
